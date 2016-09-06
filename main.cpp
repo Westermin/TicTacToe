7be056f8d58dc8ccf1e195ea31165ae2469f7e5d
Westermin <reaverwester@gmail.com>
2016-09-06 18:58:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include <stdio.h> // Should be removed later when all the stdio related stuff like prinf has been replace with c out
+#include "SerialClass.h"	// Library described above
+#include <string>
+#include <tchar.h>
 #include <iostream>
 char square[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char player[2] = {'X', 'O'};
@@ -9,13 +13,44 @@ int idx =0;
 int choice;
 int c1, c2 ,c3, c4, c5, c6, c7, c8, c9 = 0;
 int checkForWinner();
-int main()
+
+
+
+// application reads from the specified serial port and reports the collected data
+int _tmain(int argc, _TCHAR* argv[])
 {
+	printf("Welcome to the serial test app!\n\n");
+
+	Serial* SP = new Serial("\\\\.\\COM3");    // adjust as needed
+
+	if (SP->IsConnected())
+		printf("We're connected");
+
+	char incomingData[256] = "";			// don't forget to pre-allocate memory
+	//printf("%s\n",incomingData);
+	int dataLength = 255;
+	int readResult = 0;
+
+	while(SP->IsConnected())
+	{
+		readResult = SP->ReadData(incomingData,dataLength);
+		// printf("Bytes read: (0 means no data available) %i\n",readResult);
+                incomingData[readResult] = 0;
+
+        	printf("%s",incomingData);
+
+		Sleep(500);
+	}
+	return 0;
+
+
+// ACTUAL CODE *******************************************************************************
     do {
         idx = (turn%2==0) ? 1:2;
         board();
         std::cout << "Turn:" << turn << ", Player " << idx << ", enter a number:  ";
 	std::cin >> choice;
+
 	if (choice == 1 && c1 == 0)
     {
         square[choice-1] = player[idx-1];
@@ -78,7 +113,7 @@ int main()
 
 	board();
 	checkwin = checkForWinner();
-    } while(checkwin == 0);
+    } while(checkwin == -1);
     if(checkwin==1)
     {
 
@@ -88,6 +123,7 @@ int main()
     {
 
 		std::cout << "Game draw.";
+		std::cout << checkwin << std::endl;
     }
     return 0;
 }
@@ -111,23 +147,23 @@ void board()
 // returns 1 if there's a winner, 0 for a draw, -1 the game continues
 int checkForWinner()
 {
-    if (square[1] == square[2] && square[2] == square[3])
+if (square[0] == square[1] && square[1] == square[2])
 		return 1;
-	else if (square[4] == square[5] && square[5] == square[6])
+	else if (square[3] == square[4] && square[4] == square[5])
 		return 1;
-	else if (square[7] == square[8] && square[8] == square[9])
+	else if (square[6] == square[7] && square[7] == square[8])
+		return 1;
+	else if (square[0] == square[3] && square[3] == square[6])
 		return 1;
 	else if (square[1] == square[4] && square[4] == square[7])
 		return 1;
 	else if (square[2] == square[5] && square[5] == square[8])
 		return 1;
-	else if (square[3] == square[6] && square[6] == square[9])
+	else if (square[0] == square[4] && square[4] == square[8])
 		return 1;
-	else if (square[1] == square[5] && square[5] == square[9])
+	else if (square[2] == square[4] && square[4] == square[6])
 		return 1;
-	else if (square[3] == square[5] && square[5] == square[7])
-		return 1;
-	else if (square[1] != '1' && square[2] != '2' && square[3] != '3'  && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+	else if (square[0] != '1' && square[1] != '2' && square[2] != '3'  && square[3] != '4' && square[4] != '5' && square[5] != '6' && square[6] != '7' && square[7] != '8' && square[8] != '9')
 		return 0;
 	else
 		return -1;
