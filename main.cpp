6bfb791a2be804cd9c0a6475f0f0421fa369cb5b
Westermin <reaverwester@gmail.com>
2016-09-06 18:19:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,88 @@ void board();
 int checkwin = 0;
 int idx =0;
 int choice;
+int c1, c2 ,c3, c4, c5, c6, c7, c8, c9 = 0;
 int checkForWinner();
 int main()
 {
     do {
         idx = (turn%2==0) ? 1:2;
+        board();
         std::cout << "Turn:" << turn << ", Player " << idx << ", enter a number:  ";
 	std::cin >> choice;
-	square[choice-1] = player[idx-1];
+	if (choice == 1 && c1 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c1 = 1;
+        turn++;
+    }
+    else if(choice == 2 && c2 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c2 = 1;
+        c2 = 1;
+        turn++;
+    }
+    else if(choice == 3 && c3 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c3 = 1;
+        turn++;
+    }
+    else if(choice == 4 && c4 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c4 = 1;
+        turn++;
+    }
+    else if(choice == 5 && c5 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c5 = 1;
+        turn++;
+    }
+    else if(choice == 6 && c6 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c6 = 1;
+        turn++;
+    }
+    else if(choice == 7 && c7 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c7 = 1;
+        turn++;
+    }
+    else if(choice == 8 && c8 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c8 = 1;
+        turn++;
+    }
+    else if(choice == 9 && c9 == 0)
+    {
+        square[choice-1] = player[idx-1];
+        c9 = 1;
+        turn++;
+    }
+    else
+    {
+        std::cout << "Invalid move" << std::endl;
+    }
+
 	board();
 	checkwin = checkForWinner();
-	turn++;
     } while(checkwin == 0);
     if(checkwin==1)
+    {
+
 		std::cout << "The player " << idx << " win.";
+    }
 	else
+    {
+
 		std::cout << "Game draw.";
+    }
     return 0;
 }
 void board()
